4c130ecba1b3eefd23e6efab499f943bf201d043
golashr <rahul.g@ledgerium.net>
2020-03-02 22:32:48 +1100

    env fixed

--- webjetserver/config/config.js
+++ webjetserver/config/config.js
@@ -1,14 +1,12 @@
+const path = require('path');
+// eslint-disable-next-line no-path-concat
+require('dotenv').config({ path: path.resolve(__dirname + '/../.env') });
+
 module.exports = {
   server: {
     port: process.env.PORT || 3001,
     https: false
   // eslint-disable-next-line no-inline-comments
-  },
-  mongodb: {
-    host: process.env.MONGO_HOST || 'localhost',
-    port: process.env.MONGO_PORT || '27017',
-    dbName: process.env.MONGO_DB || 'webjetserver',
-    collectionName: 'webjetserver'
   },
   cinemaworldBaseURL: `${process.env.CINEMAWORLD_HOST}:${process.env.CINEMAWORLD_PORT}`,
   filmworldBaseURL: `${process.env.FILMWORLD_HOST}:${process.env.FILMWORLD_PORT}`,
