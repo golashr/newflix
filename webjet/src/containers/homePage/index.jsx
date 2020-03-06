4d713e91f1513dfb6094f42829dcc70e1e7cb021
golashr <rahul.g@ledgerium.net>
2020-03-07 00:14:38 +1100

    styled component react-table

--- webjet/src/containers/homePage/index.jsx
+++ webjet/src/containers/homePage/index.jsx
@@ -5,7 +5,80 @@ import {
   filmWorldRequestFailed
 } from './homePage.actions';
 import { getFilmMaker } from '../../api/filmmaker';
-import './styles.css';
+
+import styled from 'styled-components';
+import { useTable } from 'react-table';
+// import './styles.css';
+
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`;
+
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({
+    columns,
+    data
+  });
+
+  // Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
 
 class HomePage extends React.Component {
   async componentDidMount() {
@@ -18,20 +91,49 @@ class HomePage extends React.Component {
 
   render() {
     const { filmWorldData } = this.props;
+    const columns = [
+      {
+        Header: 'Title',
+        accessor: 'title'
+      },
+      {
+        Header: 'Owner',
+        accessor: 'owner'
+      },
+      {
+        Header: 'Charges',
+        accessor: 'charges'
+      },
+      {
+        Header: 'Broadcaster',
+        accessor: 'broadcaster'
+      },
+      {
+        Header: 'Description',
+        accessor: 'description'
+      }
+    ];
+
     return (
-      <div className="TileContainer">
-        {filmWorldData &&
-          filmWorldData.map((filmWorld, index) => {
-            return (
-              <div className="Tiles" key={index}>
-                {filmWorld.title}
-                {filmWorld.charges}
-                {filmWorld.description}
-                {filmWorld.owner}
-              </div>
-            );
-          })}
-      </div>
+      <Styles>
+        <Table columns={columns} data={filmWorldData} />
+      </Styles>
+      // <div>
+      //   <ReactTable instance />
+      // </div>
+      // <div className="TileContainer">
+      //   {filmWorldData &&
+      //     filmWorldData.map((filmWorld, index) => {
+      //       return (
+      //         <div className="Tiles" key={index}>
+      //           {filmWorld.title}
+      //           {filmWorld.charges}
+      //           {filmWorld.description}
+      //           {filmWorld.owner}
+      //         </div>
+      //       );
+      //     })}
+      // </div>
     );
   }
 }
